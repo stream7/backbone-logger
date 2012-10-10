19bae418040f264a32b4c5f3143a2c4de4fd2a67
Nikos Gereoudakis <gereoudakis@gmail.com>
2012-10-10 10:37:14 +0300

    remove shims and use underscore

--- backbone-logger.js
+++ backbone-logger.js
@@ -6,56 +6,6 @@
 (function() {
   'use strict';
 
-  if (!('forEach' in Array.prototype)) {
-    Array.prototype.forEach= function(action, that) {
-      for (var i= 0, n= this.length; i<n; i++)
-        if (i in this)
-          action.call(that, this[i], i, this);
-    };
-  }
-
-  if (!('indexOf' in Array.prototype)) {
-    Array.prototype.indexOf= function(find, i /*opt*/) {
-        if (i===undefined) i= 0;
-        if (i<0) i+= this.length;
-        if (i<0) i= 0;
-        for (var n= this.length; i<n; i++)
-            if (i in this && this[i]===find)
-                return i;
-        return -1;
-    };
-  }
-
-  Object.keys = Object.keys || (function () {
-    var hasOwnProperty = Object.prototype.hasOwnProperty,
-        hasDontEnumBug = !{toString:null}.propertyIsEnumerable("toString"),
-        DontEnums = [ 
-            'toString', 'toLocaleString', 'valueOf', 'hasOwnProperty',
-            'isPrototypeOf', 'propertyIsEnumerable', 'constructor'
-        ],
-        DontEnumsLength = DontEnums.length;
-
-    return function (o) {
-        if (typeof o != "object" && typeof o != "function" || o === null)
-            throw new TypeError("Object.keys called on a non-object");
-
-        var result = [];
-        for (var name in o) {
-            if (hasOwnProperty.call(o, name))
-                result.push(name);
-        }
-
-        if (hasDontEnumBug) {
-            for (var i = 0; i < DontEnumsLength; i++) {
-                if (hasOwnProperty.call(o, DontEnums[i]))
-                    result.push(DontEnums[i]);
-            }   
-        }
-
-        return result;
-    };
-  })();
-
   Backbone.Logger = (function() {
 
     function Logger(namespace) {
@@ -82,7 +32,7 @@
     };
 
     Logger.prototype.isForbiddenProperty = function(property) {
-      return this.forbiddenProperties.indexOf(property) !== -1
+      return _.indexOf(this.forbiddenProperties, property) !== -1
     };
 
     Logger.prototype.processDescendant = function(descendant) {
@@ -110,38 +60,13 @@
     Logger.prototype.enable = function() {
       if (this.namespace) {
         this.findDescendants(this.namespace);
-        var descendant, i, len;
+        var i, len;
         for (i = 0, len = this.descendants.length; i < len; i++) {
-          descendant = this.descendants[i];
-          this.processDescendant(descendant);
+          this.processDescendant(this.descendants[i]);
         }
       } else {
-        this.enableWithoutNamespace();
+        if (console && console.log) console.log('Backbone.Logger: Provide a namespace for your app');
       }
-    };
-
-    // works only with Coffeescript
-    Logger.prototype.enableWithoutNamespace = function() {
-      ['Model', 'View', 'Router', 'Collection'].forEach(function(object){
-
-        var original = Backbone[object].prototype.constructor;
-        Backbone[object].prototype.constructor = function(){
-
-          var that = this;
-          Object.keys(this).forEach(function(property){
-            if (typeof(that[property]) === 'function') {
-              var old = that[property];
-              that[property] = function(){
-                if (console && console.log) console.log(that, property, arguments);
-                old.apply(that, arguments);
-              }
-            }
-          });
-
-          if (console && console.log) console.log(this, 'constructor', arguments);
-          return original.apply(this, arguments)
-        };
-      });
     };
 
     Logger.VERSION = '0.0.1';
